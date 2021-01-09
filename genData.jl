9f9ad6c28b6d6ba28fe18b340edfd152d8a92586
Jaque Tolma <j.toledo.mx@gmail.com>
2021-01-09 17:16:46 -0500

    update

--- genData.jl
+++ genData.jl
@@ -3,6 +3,8 @@ using Images, Plots, ArgParse
 using DifferentialEquations, DelimitedFiles
 using Parameters: @with_kw
 
+PATH = "/N/u/jtoledom/Carbonate/Data/"
+
 function parseCommandLine()
 
         # initialize the settings (the description is for the help screen)
@@ -16,16 +18,15 @@ function parseCommandLine()
                 default = 1
             "arg2"
                 arg_type=Int
-                default = 20
+                default = 5000
             "arg3"
                 arg_type=String
                 default = "TwoSourcesRdm"
             "arg4"
                 arg_type=Int64
                 default = 1
-            "arg5"
-              arg_type=String
-              default = "/nobackup/jtoledom/DiffSolver/"
+            # "arg5"
+                # arg_type=Int
             # "arg6"
                 # arg_type=Int=
         end
@@ -53,6 +54,17 @@ function f(u,p,t)
   # dA = @. gDA + α1*BPatch - r1*A*BPatch
   dA = @. gDA - r1*A + α₁
   return dA
+end
+
+function fCPU(u,p,t)
+  α₁ = p
+  A = u
+  gMyA =  gMy * A
+  gAMx = A * gMx
+  gDA =  @. Dd*(gMyA + gAMx)
+  # dA = @. gDA + α1*BPatch - r1*A*BPatch
+  dA = @. gDA - r1*A + α₁
+  return Array(dA)
 end
 
 function min_dist_to_border(source, l)
@@ -101,6 +113,7 @@ gAMx = CuArray(Float32.(zeros(size(gMx))))
 gDA = CuArray(Float32.(zeros(size(gMx))))
 u0 = α₁
 
+
 # for i in 2840:5000
 #   @info i
 #   in_cond = genInitialCond(100, 5)
@@ -128,14 +141,12 @@ st = parsed_args["arg1"]
 stop = parsed_args["arg2"]
 dir_name = parsed_args["arg3"]
 num_sources = parsed_args["arg4"]
-PATH = parsed_args["arg5"]
 runFunction(st, stop, dir_name; ns=num_sources)
 
 
-
 ############################
-# α₁, _ = genInitialCond(100, 5; ns=500)
-# heatmap(Array(α₁))
+# α₁, _ = genInitialCond(100, 5)
+# colorview(Gray, Array(α₁))
 # gMx, gMy = gen_op(size(α₁,1))        # This generates the discrete laplacian as in CR's tutorial
 # r1 = 1.0/400
 # Dd = 1.0
@@ -164,3 +175,43 @@ runFunction(st, stop, dir_name; ns=num_sources)
 #   sum([(2/π)^2 * sin(n*π*x0/l) * sin(m*π*y0/l) * sin(n*π*x/l) *
 #     sin(m*π*y/l) * 1/d * 1/(n^2+m^2 + γ/d * l^2/pi^2) for n in 1:n0, m in 1:m0])
 # end
+
+
+#
+using BenchmarkTools
+gMx, gMy = gen_op(size(α₁,1))       # This generates the discrete laplacian as in CR's tutorial
+r1 = 1.0/400
+Dd = 1.0
+gMyA = CuArray(Float32.(zeros(size(gMx))))
+gAMx = CuArray(Float32.(zeros(size(gMx))))
+gDA = CuArray(Float32.(zeros(size(gMx))))
+
+in_cond, dist = genInitialCond(100, 5; ns=2)
+α₁ = in_cond
+u0 = α₁
+heatmap(in_cond|>cpu)
+prob = ODEProblem{false}(f,u0,(0.0,3000.0),α₁)
+@time sol = solve(prob,ROCK2(),progress=false,save_everystep=false,save_start=false)
+out = Array(sol[end])/maximum(sol[end])
+heatmap(out)
+
+
+
+#cpu
+in_cond, dist = genInitialCond(100, 5; ns=2)
+α₁ = Array(in_cond)
+u0 = Array(α₁)
+heatmap(in_cond|>cpu)
+prob = ODEProblem{false}(fCPU,u0,(0.0,3000.0),α₁)
+@time sol = solve(prob,ROCK2(),progress=false,save_everystep=false,save_start=false)
+out = Array(sol[end])/maximum(sol[end])
+heatmap(out)
+
+
+
+gMx, gMy = Array.(gen_op(size(α₁,1)))        # This generates the discrete laplacian as in CR's tutorial
+r1 = 1.0/400
+Dd = 1.0
+gMyA = Array(Float32.(zeros(size(gMx))))
+gAMx = Array(Float32.(zeros(size(gMx))))
+gDA = Array(Float32.(zeros(size(gMx))))
